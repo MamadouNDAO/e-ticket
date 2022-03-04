e804fa141d2996f5761168d3bde3943c9928c55d
Mamadou NDAO [SNT DST/DD/DPD/PMA] <Mamadou.NDAO@orange-sonatel.com>
2022-03-04 22:34:59 +0000

    to mianne

--- fonction/bdd.php
+++ fonction/bdd.php
@@ -5,7 +5,7 @@
         $host = "localhost";
         $dbusername = "root";
         $pass = "";
-        $bdd = "eticket";
+        $bdd = "e-ticket";
         $conn = new PDO("mysql:host=$host;dbname=$bdd", $dbusername, $pass);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     } catch (Exception $e) {
